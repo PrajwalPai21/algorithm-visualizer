853d7532fb4e7a219628515dd282016212eeb1be
Prajwal Pai <prajwalpai21@gmail.com>
2026-01-27 18:12:43 +0530

    Can sort bars (only 1 pass)

--- AlgorithmVisualizer/src/main/java/com/algovis/algorithmvisualizer/AlgoVisApplication.java
+++ AlgorithmVisualizer/src/main/java/com/algovis/algorithmvisualizer/AlgoVisApplication.java
@@ -2,7 +2,6 @@ package com.algovis.algorithmvisualizer;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.application.Application;
 //import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -12,15 +11,15 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.io.IOException;
+
 import javafx.application.Application;
 
 public class AlgoVisApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         Pane root = new Pane();
 
-        int[] values = {10,20,30,40,50}; //Height of the bars
+        int[] values = {20,50,10,30,40}; //Height of the bars
         int x = 20;
         Rectangle[] bars = new Rectangle[values.length];
 
@@ -31,27 +30,59 @@ public class AlgoVisApplication extends Application {
             root.getChildren().add(bar);
             x+=50; //Gap between each bar
         }
-        int i = 1;
-        bars[i].setFill(Color.RED);
-        bars[i+1].setFill(Color.RED);
 
-        Timeline timeline = new Timeline(
-                new KeyFrame(Duration.seconds(2), event -> {
-                    // Swap Bars after 2 seconds ,
-                    double x1 = bars[i].getX();
-                    double x2 = bars[i+1].getX(); //Swap Code
+//        int i = 1;
+//        bars[i].setFill(Color.RED);
+//        bars[i+1].setFill(Color.RED);
 
-                    bars[i].setX(x2);
-                    bars[i+1].setX(x1);
+        int[] j = {0};
+        Timeline[] timeline = new Timeline[1];
+        timeline[0] = new Timeline(
+                new KeyFrame(Duration.seconds(0.5), event -> {
+//                    Stop condition to prevent exception
+                    if (j[0] >= values.length - 1) {
+                        timeline[0].stop();
+                        return;
+                    }
+//                    Resetting the colors of the bars
+
+                    for(Rectangle bar : bars){
+                        bar.setFill(Color.BLUE);
+                    }
+//                    highlight the bars that are being compared
+                    bars[j[0]].setFill(Color.RED);
+                    bars[j[0]+1].setFill(Color.RED);
+
+//                    Swap values in the array
+                    if(values[j[0]] > values[j[0] + 1]) {
+                        int temp = values[j[0]];
+                        values[j[0]] = values[j[0] + 1];
+                        values[j[0] + 1] = temp;
+
+//                    Swap bar positions
+                        double x1 = bars[j[0]].getX();
+                        double x2 = bars[j[0] + 1].getX();
+                        bars[j[0]].setX(x2);
+                        bars[j[0] + 1].setX(x1);
+
+//                    Swap bars visually
+                        Rectangle tempBar = bars[j[0]];
+                        bars[j[0]] = bars[j[0] + 1];
+                        bars[j[0] + 1] = tempBar;
+                    }
+//                    next comparison
+                    j[0]++;
+//                    stop after one pass
                 })
-        );
 
-        timeline.play();
+        );
+        timeline[0].setCycleCount(Timeline.INDEFINITE);
+        timeline[0].play();
 
         Label label = new Label("TEST");
         root.getChildren().add(label);
 
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 400, 300);
         stage.setScene(scene);
         stage.setTitle("Algorithm Visualizer");
 
@@ -59,9 +90,9 @@ public class AlgoVisApplication extends Application {
     }
 }
 
-//Rectangle rect = new Rectangle(100, 100, 50, 100);
-//rect.setFill(Color.gray(0.5));
-//root.getChildren().add(rect);
+//        Rectangle rect = new Rectangle(100, 100, 50, 100);
+//        rect.setFill(Color.gray(0.5));
+//        root.getChildren().add(rect);
 
 
 
@@ -71,3 +102,14 @@ public class AlgoVisApplication extends Application {
 //
 //        bars[i].setX(x2);
 //        bars[i+1].setX(x1);
+
+
+
+/*
+for (Type variable : collection) {
+// use variable }
+for (i = 0; i < bars.length; i++) {
+Rectangle bar = bars[i];
+bar.setFill(Color.CORNFLOWERBLUE);
+} the below for loop does the same
+*/
