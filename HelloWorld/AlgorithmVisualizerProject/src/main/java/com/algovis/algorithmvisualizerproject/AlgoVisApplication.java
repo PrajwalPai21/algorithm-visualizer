55448d404e9699e2df110203ca69117a0556a9e0
Prajwal Pai <prajwalpai21@gmail.com>
2026-01-27 13:42:55 +0530

    Basic Timeline/Animation testing

--- HelloWorld/AlgorithmVisualizerProject/src/main/java/com/algovis/algorithmvisualizerproject/AlgoVisApplication.java
+++ HelloWorld/AlgorithmVisualizerProject/src/main/java/com/algovis/algorithmvisualizerproject/AlgoVisApplication.java
@@ -1,12 +1,16 @@
 package com.algovis.algorithmvisualizerproject;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 //import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -26,12 +30,30 @@ public class AlgoVisApplication extends Application {
             root.getChildren().add(bar);
             x+=50; //Gap between each bar
         }
+        int i = 1;
+        bars[i].setFill(Color.RED);
+        bars[i+1].setFill(Color.RED);
 
-        bars[1].setFill(Color.WHITESMOKE);
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(2), event -> {
+                    // Swap Bars after 2 seconds ,
+                    double x1 = bars[i].getX();
+                    double x2 = bars[i+1].getX(); //Swap Code
+
+                    bars[i].setX(x2);
+                    bars[i+1].setX(x1);
+                })
+        );
+
+        timeline.play();
+
+        Label label = new Label("TEST");
+        root.getChildren().add(label);
+
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Algorithm Visualizer");
 
-        stage.setTitle("AlgoVis");
         stage.show();
     }
 }
@@ -39,3 +61,12 @@ public class AlgoVisApplication extends Application {
 //Rectangle rect = new Rectangle(100, 100, 50, 100);
 //rect.setFill(Color.gray(0.5));
 //root.getChildren().add(rect);
+
+
+
+//        Swapping Positions
+//        double x1 = bars[i].getX();
+//        double x2 = bars[i+1].getX(); //Swap Code
+//
+//        bars[i].setX(x2);
+//        bars[i+1].setX(x1);
