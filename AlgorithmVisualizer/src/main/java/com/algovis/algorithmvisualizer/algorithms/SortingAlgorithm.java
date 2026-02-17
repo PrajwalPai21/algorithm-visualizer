3a6b7572f2870e105ba331033487a032b577b9c1
Prajwal Pai <prajwalpai21@gmail.com>
2026-02-18 01:03:04 +0530

    Checkpoint 2

--- AlgorithmVisualizer/src/main/java/com/algovis/algorithmvisualizer/algorithms/SortingAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package com.algovis.algorithmvisualizer.algorithms;
-
-public interface SortingAlgorithm {
-    void step();
-    boolean isFinished();
-    String getAlgorithmName();
-    int getComparisons();
-}
